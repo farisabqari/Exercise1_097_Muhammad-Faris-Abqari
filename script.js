437bf71d4c33f34aa6472ef93f087656603b9fee
farisabqari <127277279+farisabqari@users.noreply.github.com>
2024-03-31 18:42:02 +0800

    first

---
+++ script.js
@@ -0,0 +1,15 @@
+function showAlert(){
+    alert(Date(),tampil(),show())
+    Date()
+
+    function tampil(){
+        alert("aduh jaringan kamu lelet")
+    }
+
+    function show(){
+        alert("nanti datang lagi dan coba lagi ya")
+    }
+
+
+
+}
